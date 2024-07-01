3a37a0385d5551cc8eca60f8ebf4a3f86fb9462f
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-01 12:55:05 +0300

    complete code challenge

--- challenge1.js
+++ challenge1.js
@@ -1,20 +1,27 @@
 
 // Define function that determines grade based on the studentmarks 
 function grade() {
-    prompt('ENTER MARKS');
+    let marks = prompt('ENTER MARKS'); //Prompts user to input marks
+    marks = Number(marks); //Converts marks to number
+    if (isNaN(marks) || marks<0 || marks>100){
+        alert('ENTER VALID MARKS BETWEEN 0-100')
+    }
     if (marks >= 80 && marks <= 100) {
         console.log("A")
+        alert('A')
     } else if (marks >= 60 && marks <= 79) {
         console.log("B")
+        alert('B')
     } else if (marks >= 50 && marks <= 59) {
         console.log("C")
+        alert('C')
     } else if (marks >= 40 && marks <= 49) {
         console.log ("D")
+        alert('D')
     } else if (marks >= 0 && marks <= 39) {
         console.log("E")
-    } else {
-        console.log ("Please Input Valid Marks")
+        alert('E')
+    } 
     }
-    }
-
+;
 grade();
