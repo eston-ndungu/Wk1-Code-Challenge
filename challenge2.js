cb31faa17101822fcf16f117b7ac31656ab81914
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-01 22:04:01 +0300

    complete code challenge

--- challenge2.js
+++ challenge2.js
@@ -1,5 +1,13 @@
 // Define a function that takes in speed of car
-function speedDetector(speed) {
+function speedDetector() { 
+    let speed = prompt('ENTER SPEED') 
+    // validate Speed
+    if(isNaN(speed) || speed<0){
+        console.log('INPUT A VALID SPEED');
+        alert('INPUT A VALID SPEED')
+        return;
+    }
+
     //Define speed limit
     const speedLimit = 70
     //Numnber of km per demerit point
@@ -8,22 +16,21 @@ function speedDetector(speed) {
     //Speed is within the speed limit, print ok
     if (speed <=  70) {
         console.log("Ok")
+        alert('Ok')
 }   else if (speed > 70 ) {
     //Calculate demerit points
      const demeritPoints = (speed - speedLimit ) / kmPerDemeritPoint
     //Print licence suspended if demerit points > 1
     if (demeritPoints > 12) {
         console.log("Licence Suspended")
+        alert('Licence Suspended')
     } else {
         // Else print number of demerit points
         console.log(`Points: ${demeritPoints}`)
+        alert(`Points: ${demeritPoints}`)
     }
 
 }
 }
 
-//Call function with different arguments
-speedDetector(180) //Prints Licence Suspended
-speedDetector(30) //Prints Ok
-speedDetector(80) //Prints Points: 2
-speedDetector(70) // Prints Ok
+speedDetector()
