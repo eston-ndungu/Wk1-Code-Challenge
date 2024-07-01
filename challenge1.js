bc14dc5f1bddf1e36e3940e87906143fadca179e
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-01 12:30:25 +0300

    complete lab

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,7 @@
 
 // Define function that determines grade based on the studentmarks 
-function grade(marks) {
-    
+function grade() {
+    prompt('ENTER MARKS');
     if (marks >= 80 && marks <= 100) {
         console.log("A")
     } else if (marks >= 60 && marks <= 79) {
@@ -17,19 +17,4 @@ function grade(marks) {
     }
     }
 
-
-
-
-
-        
-    
-
-
-// Calling the function using arguments
-grade (86.7) // Prints Grade "A"
-grade(65) // Prints Grade "B"
-grade(49) // Prints Grade "D"
-grade(55) // Prints Grade "C"
-grade (15) // Prints Grade "E"
-grade(200) // Prints "Please Input Valid Marks"
-grade (-30) // Prints "Please Input Valid Marks" 
+grade();
